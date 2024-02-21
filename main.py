88edad0d6f39a9db6c1693b0eeec3db5442a4af2
connork116 <cking6@zagmail.gonzaga.edu>
2024-02-21 12:54:43 -0800

    initial

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+def number_guessing_game():
+    rand_number = random.randint(1, 20)
+
+    print("Try to guess the number that is between 1 and 20")
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        
+        if guess == rand_number:
+            print("Good job! You guessed the number correctly!")
+            break
+        elif guess < rand_number:
+            print("Too low. Guess again.")
+        else:
+            print("Too high. Guess again.")
+
+number_guessing_game()
